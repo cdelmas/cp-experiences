b75e3cca425e9d7a7708026de0cb468b107a0930
cdelmas <cyril.delmas@gmail.com>
2019-08-20 14:11:34 +0200

    refactor: split Triangle example

--- src/main/scala/example/Triangle2.scala
+++ src/main/scala/example/Triangle2.scala
@@ -2,7 +2,7 @@ package example
 
 import oscar.cp._
 
-object Triangle extends CPModel with App {
+object Triangle2 extends CPModel with App {
 
   val max = 200
 
@@ -14,11 +14,14 @@ object Triangle extends CPModel with App {
   add(X * X + Y * Y === H * H)
   add(X + Y + H < 200)
 
+  val A = X * Y
+  maximize(A)
+
   search {
-    binaryLastConflict(Array(X, Y, H))
+    binarySplit(Seq(X,Y,H))
   } onSolution {
     println(
-      s"Solution found, value of (X,Y,H) in this solution: + (${X.value},${Y.value},${H.value})}"
+      s"Solution found, value of (X,Y,H) in this solution: + (${X.value},${Y.value},${H.value}), area = ${(X.value * Y.value) / 2}"
     )
   }
 
